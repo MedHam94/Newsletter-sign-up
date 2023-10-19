e55fe14071f7b5b5ccbd9252227437940502910c
MedHam <hammas.m.mh@gmail.com>
2023-10-19 19:26:14 +0100

    finish the media query of the main design

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,16 @@
 // let email = document.querySelector("button").value;
 // let errorMsg = document.querySelector("#errMsg");
+
 function validateEmail() {
   let email = document.getElementById("email");
 
   const errMsg = document.getElementById("errMsg");
+
   if (!email.checkValidity()) {
     errMsg.style.display = "block";
     email.style.background = "#ffe8e6";
-    email.style.color = "#ff0000";
-    email.style.borderColor = "red";
+    email.style.color = "hsl(4, 100%, 67%)";
+    email.style.borderColor = "hsl(4, 100%, 67%)";
   } else {
     errMsg.style.display = "none";
     email.style.background = "";
